d6cab830ddb7ce0e83299ffd25763196a9b7d1f6
Mridul2820 <immridulpanda2820@gmail.com>
2022-08-25 23:12:59 +0530

    limit and skip in api

--- pages/api/characters/index.js
+++ pages/api/characters/index.js
@@ -5,9 +5,40 @@ const handler = async (req, res) => {
     res.status(405);
   }
 
-  const characters = await prisma.st_character.findMany();
+  try {
+    const limit = req.query.limit;
+    const limitStr = Array.isArray(limit) ? limit[0] : limit;
 
-  res.status(200).json(characters);
+    const skip = req.query.skip;
+    const skipStr = Array.isArray(skip) ? skip[0] : skip;
+    const queryConfig = {};
+
+    if (limitStr) {
+      const take = parseInt(limitStr);
+      if (isNaN(take)) {
+        return res
+          .status(400)
+          .json({ err: "Please enter a valid number for the limit parameter" });
+      }
+      queryConfig.take = take;
+    }
+
+    if (skipStr) {
+      const skip = parseInt(skipStr);
+      if (isNaN(skip)) {
+        return res
+          .status(400)
+          .json({ err: "Please enter a valid number for the skip parameter" });
+      }
+      queryConfig.skip = skip;
+    }
+
+    const characters = await prisma.st_character.findMany(queryConfig);
+    return res.status(200).json(characters);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ err: "Aghhhhh" });
+  }
 };
 
 export default handler;
