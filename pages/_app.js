b3e96c64e4c84c37f895c3e1b81ad98e16f90512
Mridul2820 <immridulpanda2820@gmail.com>
2022-09-19 21:54:47 +0530

    scroll top add

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,15 @@
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import ScrollTop from '../components/ScrollTop';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="bg-slate-900 text-white ">
+    <div className="bg-slate-900 text-white relative">
       <Header />
       <Component {...pageProps} />
       <Footer />
+      <ScrollTop />
     </div>
   );
 }
