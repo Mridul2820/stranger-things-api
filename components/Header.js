8755d7ccc8923bfa0ccaadbe85e811ed3bc898c7
Mridul2820 <immridulpanda2820@gmail.com>
2022-09-19 19:14:25 +0530

    header github link add

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { AiFillGithub } from 'react-icons/ai';
 
 const Header = () => {
   return (
     <div className="flex justify-between items-center px-5 py-3">
-      <img
-        src="/assets/st_api_logo.jpg"
-        alt="logo"
-        className="h-10 w-10 rounded-sm"
-      />
+      <a href="/" title="Home">
+        <img
+          src="/assets/st_api_logo.jpg"
+          alt="logo"
+          className="h-10 w-10 rounded-sm"
+        />
+      </a>
+
       <div className="flex items-center space-x-3">
-        <a href="/" className="btn">
+        <a href="/" className="btn" title="Home">
           Home
         </a>
         <a
@@ -17,8 +21,17 @@ const Header = () => {
           className="btn"
           target="_blank"
           rel="noreferrer"
+          title="Stranger Things API Docs"
         >
           API Docs
+        </a>
+        <a
+          href="https://github.com/Mridul2820/stranger-things-api"
+          target="_blank"
+          rel="noreferrer"
+          title="Github"
+        >
+          <AiFillGithub size={26} />
         </a>
       </div>
     </div>
