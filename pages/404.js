e7f1032534b9459544ddb9157cc9405b523e5eb8
Mridul2820 <immridulpanda2820@gmail.com>
2022-09-20 13:48:24 +0530

    404 and 500 page

---
+++ pages/404.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className="min-h-[calc(100vh-130px)] flex flex-col justify-center items-center gap-4">
+      <h1 className="text-9xl font-bold text-center">404</h1>
+      <h2 className="text-3xl font-bold text-center">
+        Something More Stranger
+      </h2>
+      <p className="text-center">Sorry, Page Not Found</p>
+      <a href="/" className="btn">
+        Go to Home
+      </a>
+      <img
+        src="/assets/st_api_logo.jpg"
+        alt="404"
+        className="w-36 h-36 rounded-md align-top"
+      />
+    </div>
+  );
+};
+
+export default NotFound;
